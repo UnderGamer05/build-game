2cf2811d854ee4fe7bb502f385c1974f29a07a55
Ap <94882246+UnderGamer05@users.noreply.github.com>
2024-06-22 09:35:26 +0600

    Update build-game.sh

--- build-game.sh
+++ build-game.sh
@@ -5,6 +5,8 @@ read -p "Enter the game name (without extension): " GAME_NAME
 
 cd "$PROJECT_PATH" || { echo "Project path not found. Exiting."; exit 1; }
 
+clear
+
 ~/godot/Godot_v4.2.2-stable_linux.arm32 --export-release "Android" ./"$GAME_NAME".apk --headless
 
 echo ""
@@ -15,5 +17,5 @@ echo "If you face any problem or if you have any questions than please visit htt
 
 echo ""
 echo "Thanks To UnderGamer"
-echo "Ap Games Studio"
+echo ""
 
