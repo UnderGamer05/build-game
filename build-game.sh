281800a19154ad5e0d23c10ad0ec82edb70afafe
Ap <94882246+UnderGamer05@users.noreply.github.com>
2024-06-26 11:38:13 +0600

    Update build-game.sh

--- build-game.sh
+++ build-game.sh
@@ -7,7 +7,12 @@ cd "$PROJECT_PATH" || { echo "Project path not found. Exiting."; exit 1; }
 
 clear
 
-~/godot/Godot_v4.2.2-stable_linux.arm32 --export-release "Android" ./"$GAME_NAME".apk --headless
+cd ~/godot
+chmod +x Godot_v4.2.2-stable_linux.arm32
+
+Godot_v4.2.2-stable_linux.arm32 --export-release "Android" ./"$GAME_NAME".apk --headless
+
+cd ~
 
 echo ""
 echo "Run Compled."
